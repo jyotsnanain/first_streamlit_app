3f73f681732004a7bc84300023fd8a2a7d6ceece
jyotsnanain <96388151+jyotsnanain@users.noreply.github.com>
2023-04-09 14:46:58 +0530

    Update Streamlit_app.py
    created

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,3 +1,2 @@
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+import streamlit
+streamlit.title('My Parents New Healthy Diner')
